a8fa82cfadbf3a4e67ab6a1758fd0a7994f06971
Danu <danu.mds1402@gmail.com>
2020-11-01 16:57:43 +0700

    New features  notification app

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,17 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:sikeupaud/authservices.dart';
 import 'package:sikeupaud/wrapper.dart';
+import 'package:onesignal_flutter/onesignal_flutter.dart';
 
-void main() => runApp(MyApp());
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  OneSignal.shared
+      .init("4e264f94-f0a1-41b6-9366-02d3a7375534", iOSSettings: null);
+  OneSignal.shared
+      .setInFocusDisplayType(OSNotificationDisplayType.notification);
+  runApp(MyApp());
+}
 
 class MyApp extends StatelessWidget {
   @override
@@ -15,5 +24,42 @@ class MyApp extends StatelessWidget {
         home: Wrapper(),
       ),
     );
+  }
+}
+
+class Mainpage extends StatefulWidget {
+  @override
+  _MainpageState createState() => _MainpageState();
+}
+
+class _MainpageState extends State<Mainpage> {
+  String title = "title";
+  String content = 'content';
+  @override
+  void initState() {
+    super.initState();
+
+    OneSignal.shared
+        .setNotificationReceivedHandler((OSNotification notification) {
+      setState(() {
+        title = notification.payload.title;
+        content = notification.payload.body;
+      });
+    });
+
+    OneSignal.shared
+        .setNotificationOpenedHandler((OSNotificationOpenedResult result) {
+      print("Notifikasi Di pencet");
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Center(
+      child: Column(
+        children: [Text(title), Text(content)],
+      ),
+    ));
   }
 }
